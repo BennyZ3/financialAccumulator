0078eedcf1b8c72ed2445c94f0cd24cb8c7b04ab
BennyZ3 <bennyz3@hotmail.com>
2021-08-12 11:52:17 -0400

    functions finished without edge case testing

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const currencies = require("./data/currencies.json");
 
 // Issue: toFixed returns a string, so we have to convert it back to a number.
 // This is a helper function that rounds to two decimal plaes and converts that string back to a number.
-function roundNum(num) {}
+function roundNum(num) {
+    return (Math.round(Number(num)*100)/100)
+}
 
 // console.log(roundNum(3.1415));
 // >> 3.14
@@ -17,57 +19,113 @@ function roundNum(num) {}
 // >> 'number'
 
 // Find the total price of all trades.
-function findTotalPrice(trades) {}
+function findTotalPrice(trades) {
+    let total = 0
+    for (let trade of trades){
+        total += Number(trade.price) //* Number(trade.size)
+    }
+    return roundNum(total)
+}
 
-// console.log(findTotalPrice(bitcoinTrades));
+console.log(findTotalPrice(bitcoinTrades));
 // >> 41815183.19
 
 // Find the highest price of all trades.
-function findHighestPrice(trades) {}
+function findHighestPrice(trades) {
+    let highest = Number(trades[0].price)
+    for (let i = 1; i< trades.length; i++){
+        if (Number(trades[i].price) > highest){
+           highest = Number(trades[i].price) 
+        }
+    }
+    return roundNum(highest)
+}
 
-// console.log(findHighestPrice(bitcoinTrades));
+console.log(findHighestPrice(bitcoinTrades));
 // >> 41850.03
 
 // Find the average price of all trades.
-function findAveragePrice(trades) {}
+function findAveragePrice(trades) {
+    return roundNum(findTotalPrice(trades)/trades.length)
+}
 
-// console.log(findAveragePrice(bitcoinTrades));
+console.log(findAveragePrice(bitcoinTrades));
 // >> 41815.18
 
 // Find the lowest price of all trades.
-function findLowestPrice(trades) {}
+function findLowestPrice(trades) {
+    let lowest = Number(trades[0].price)
+    for (let i = 1; i< trades.length; i++){
+        if (Number(trades[i].price) < lowest){
+           lowest = Number(trades[i].price) 
+        }
+    }
+    return roundNum(lowest)
+}
 
-// console.log(findLowestPrice(bitcoinTrades));
+console.log(findLowestPrice(bitcoinTrades));
 // >> 41781.9
 
 // Find the total size of all trades.
-function findTotalSize(trades) {}
+function findTotalSize(trades) {
+    let total = 0
+    for (let trade of trades){
+        total += Number(trade.size) //* Number(trade.size)
+    }
+    return roundNum(total)
+}
 
-// console.log(findTotalSize(bitcoinTrades));
+console.log(findTotalSize(bitcoinTrades));
 // >> 106.58
 
 // Find the highest size of all trades.
-function findHighestSize(trades) {}
+function findHighestSize(trades) {
+    let highest = Number(trades[0].size)
+    for (let i = 1; i< trades.length; i++){
+        if (Number(trades[i].size) > highest){
+           highest = Number(trades[i].size) 
+        }
+    }
+    return roundNum(highest)
+}
 
-// console.log(findHighestSize(bitcoinTrades));
+console.log(findHighestSize(bitcoinTrades));
 // >> 2.34
 
 // Find the average size of all trades.
-function findAverageSize(trades) {}
+function findAverageSize(trades) {
+    return roundNum(findTotalSize(trades)/trades.length)
+}
 
-// console.log(findAverageSize(bitcoinTrades));
+console.log(findAverageSize(bitcoinTrades));
 // >> 0.11
 
 // Find the lowest size of all trades.
-function findLowestSize(trades) {}
+function findLowestSize(trades) {
+    let lowest = Number(trades[0].size)
+    for (let i = 1; i< trades.length; i++){
+        if (Number(trades[i].size) < lowest){
+           lowest = Number(trades[i].size) 
+        }
+    }
+    return roundNum(lowest)
+}
 
-// console.log(findLowestSize(bitcoinTrades));
+console.log(findLowestSize(bitcoinTrades));
 // >> 0
 
 // Filter the trades by trade type i.e. buy/sell. If type is not provided do not filter. If type is an invalid input return an empty array.
-function getTradeType(trades, type) {}
+function getTradeType(trades, type) {
+    let result = []
+    for (let trade of trades){
+        if (trade.side === type){
+            result.push(trade)
+        }
+    }
+    return result
+}
 
-// console.log(getTradeType(bitcoinTrades, 'buy'));
+console.log(getTradeType(bitcoinTrades, 'buy'));
 // >> [
 //      {
 //        "time": "2021-08-06T15:50:36.4683Z",
@@ -93,9 +151,20 @@ function getTradeType(trades, type) {}
 // ]
 
 // Get all the info (total price, average size, etc.) of all trades.
-function getFullInfo(trades) {}
+function getFullInfo(trades) {
+    return {
+    totalPrice: findTotalPrice(trades),
+    totalSize: findTotalSize(trades),
+    averagePrice: findAveragePrice(trades),
+    averageSize: findAverageSize(trades),
+    highestPrice: findHighestPrice(trades),
+    lowestPrice: findLowestPrice(trades),
+    highestSize: findHighestSize(trades),
+    lowestSize: findLowestSize(trades)
+}
+}
 
-// console.log(getFullInfo(bitcoinTrades));
+console.log(getFullInfo(bitcoinTrades));
 // >> {
 //     totalPrice: 41815183.19,
 //     totalSize: 106.58,
@@ -108,9 +177,18 @@ function getFullInfo(trades) {}
 // }
 
 // Return an object that contains the currency type as the key and an array of names/types strings as the value. See return example below for formatting.
-function categorizeCurrencies(currencies) {}
+function categorizeCurrencies(currencies) {
+    let result = {}
+    for (let item of currencies){
+        if (!Object.keys(result).includes(item.details.type)){
+            result[item.details.type] = []
+        }
+        result[item.details.type].push(`${item.name}(${item.id})`)
+    }
+    return result
+}
 
-// console.log(categorizeCurrencies(currencies));
+console.log(categorizeCurrencies(currencies));
 // {
 //     crypto: [
 //       'Dash(DASH)',
